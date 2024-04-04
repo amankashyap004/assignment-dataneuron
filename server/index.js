8b0717078054235f1d6e0fdd809dd628d466dc88
amankashyap004 <amankashyap004@gmail.com>
2024-04-05 00:01:42 +0530

    create: backend features

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./db/index.js";
+import routers from "./routes/user.routes.js";
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", routers);
+
+app.listen(process.env.PORT, () => {
+  console.log("server is runing...");
+});
